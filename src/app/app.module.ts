343dcfc89d90b4d28deaa33348a1f51db847c039
Fhellipe Valentin <fhellipereis@outlook.com>
2022-04-10 17:48:32 -0300

    CORS: Salvando clientes atraves da Aplicacao WEB para a REST API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -16,6 +17,7 @@ import { ClientesService } from './clientes.service';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     TemplateModule,
     ClientesModule
