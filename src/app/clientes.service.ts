27d27eefe2f8b935cf95268a28cb6a5b45e25fe5
Fhellipe Valentin <fhellipereis@outlook.com>
2022-04-11 18:07:49 -0300

    Criando o componente de listagem de clientes

--- src/app/clientes.service.ts
+++ src/app/clientes.service.ts
@@ -16,10 +16,16 @@ salvar (cliente: Cliente) : Observable<Cliente> {
 
 }
 
-getCliente() : Cliente {
-  let cliente : Cliente = new Cliente();
-  cliente.nome = 'Fulano de tal';
-  cliente.cpf = '88888888888'
-  return cliente;
-  }
+/*getClientes() : Observable<Cliente[]> {
+  return []; 
+}*/
+
+getClientes() : Cliente[] {
+  let cliente = new Cliente();
+  cliente.id = 1;
+  cliente.nome = 'Scruto';
+  cliente.dataCadastro = '18/04/2020';
+  cliente.cpf = '12345678901';
+  return [cliente];
+}
 }
