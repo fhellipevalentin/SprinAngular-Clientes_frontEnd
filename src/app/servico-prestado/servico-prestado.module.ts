44570a09b74ec688b75babeca5d1f9174434d880
Fhellipe Valentin <fhellipereis@outlook.com>
2022-04-17 15:24:16 -0300

    Formulario de Servicos Prestados Pt.1

--- src/app/servico-prestado/servico-prestado.module.ts
+++ src/app/servico-prestado/servico-prestado.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'
 
 import { ServicoPrestadoRoutingModule } from './servico-prestado-routing.module';
 import { ServicoPrestadoFormComponent } from './servico-prestado-form/servico-prestado-form.component';
 import { ServicoPrestadoListaComponent } from './servico-prestado-lista/servico-prestado-lista.component';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
@@ -13,7 +15,9 @@ import { ServicoPrestadoListaComponent } from './servico-prestado-lista/servico-
   ],
   imports: [
     CommonModule,
-    ServicoPrestadoRoutingModule
+    ServicoPrestadoRoutingModule,
+    FormsModule,
+    RouterModule
   ],
   exports: [
     ServicoPrestadoFormComponent,
