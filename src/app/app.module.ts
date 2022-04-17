1a730299975e16818e4d17daa9d8ec93f4199202
Fhellipe Valentin <fhellipereis@outlook.com>
2022-04-17 16:37:27 -0300

    Servicos prestados e variaveis de ambiente utilizando Environment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { ClientesModule } from './clientes/clientes.module';
 
 import { ClientesService } from './clientes.service';
 import { ServicoPrestadoModule } from './servico-prestado/servico-prestado.module';
+import { ServicoPrestadoService } from './servico-prestado.service';
 
 @NgModule({
   declarations: [
@@ -22,10 +23,11 @@ import { ServicoPrestadoModule } from './servico-prestado/servico-prestado.modul
     AppRoutingModule,
     TemplateModule,
     ClientesModule,
-    ServicoPrestadoModule
+    ServicoPrestadoModule,
   ],
   providers: [
-    ClientesService
+    ClientesService,
+    ServicoPrestadoService
   ],
   bootstrap: [AppComponent]
 })
