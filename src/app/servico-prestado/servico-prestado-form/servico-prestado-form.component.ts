31c8dff3108fbdbe3a992cfb9cdf78e434967fc5
Fhellipe Valentin <fhellipereis@outlook.com>
2022-04-18 18:26:46 -0300

    Salvando o Servico conectando com a API - alteracoes no formulario

--- src/app/servico-prestado/servico-prestado-form/servico-prestado-form.component.ts
+++ src/app/servico-prestado/servico-prestado-form/servico-prestado-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ClientesService } from 'src/app/clientes.service';
 import { Cliente } from 'src/app/clientes/cliente';
+import { ServicoPrestadoService } from 'src/app/servico-prestado.service';
 import { ServicoPrestado } from '../servicoPrestado';
 
 @Component({
@@ -13,7 +14,7 @@ export class ServicoPrestadoFormComponent implements OnInit {
   clientes: Cliente[] = [];
   servico: ServicoPrestado;
 
-  constructor(private clienteService: ClientesService) { 
+  constructor(private clienteService: ClientesService, private service: ServicoPrestadoService) { 
     this.servico = new ServicoPrestado();
   }
 
@@ -24,6 +25,10 @@ export class ServicoPrestadoFormComponent implements OnInit {
   }
 
   onSubmit() {
-    console.log(this.servico)
+    this.service
+      .salvar(this.servico)
+      .subscribe( response =>{
+        console.log(response);
+      })
   }
 }
